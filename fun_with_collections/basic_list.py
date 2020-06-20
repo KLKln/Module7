88ecb6178b80e46fedcffd1e30164836eb0dc483
Kelly Klein <kmklein2@dmacc.edu>
2020-06-20 14:14:46 -0500

    update basic_list

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,11 +1,26 @@
 
 def make_list():
-    for x in range(2):
-        get_input()
-    pass
+    user_num = []
+    user_number = get_input()
+    print('a is set to:', 5)
+    while True:
+        try:
+            for x in range(2):
+                user_number
+                if user_number.isdigit():
+                    user_num.append(user_number)
+                    break
+                else:
+                    raise TypeError
+        except TypeError:
+            print('Numbers only please.')
+            continue
+
+    return user_num
 
 
 def get_input():
+
 
     pass
 
