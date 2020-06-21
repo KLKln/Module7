e722a93c4db701d63a5f737816a4551275a40f38
Kelly Klein <kmklein2@dmacc.edu>
2020-06-21 16:34:30 -0500

    updated reST

--- fun_with_collections/sort_and_search_list.py
+++ fun_with_collections/sort_and_search_list.py
@@ -2,12 +2,18 @@
 Program:sort_and_search_list.py
 Author: Kelly Klein
 Last date modified: 6/21/2020
-This program will take user input of numbers and store them in a list.
+This program will take user input to search a list for a number
+and also sort the list.
 """
 
 user_list = [21, 31, 56, 78, 22, 4, 13, 42, 9]
 
 def search_list(user_list):
+    """
+    Use reST style.
+    :param user_list: list passed in
+    :return: the index of the number user chooses, or -1 if not found
+    """
     #return the index of the object in the list
     number_search = int(input('What number would you like to search for: '))
     for i in range(len(user_list)):
@@ -19,11 +25,13 @@ def search_list(user_list):
             return print(-1)
 
 
-
     # or -1 if not found
-
-
 def sort_list(user_list):
+    """
+    Use reST style.
+    :param user_list: list passed in
+    :return: the list sorted
+    """
     #sorts the list
     user_list.sort() #no return because it calls a built in python function
     print(user_list)
