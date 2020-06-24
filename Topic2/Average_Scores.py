0d9dcf5afea26302cc66a6fd16740571a60f8bfa
Kelly Klein <kmklein2@dmacc.edu>
2020-06-24 16:30:08 -0500

    Update Average_Scores.py

--- Topic2/Average_Scores.py
+++ Topic2/Average_Scores.py
@@ -1,3 +1,4 @@
+
 def average_scores_args_only(*args):
     # Use *args for average calculation
     result = 0
@@ -6,29 +7,28 @@ def average_scores_args_only(*args):
         average = sum(args) / len(args)
         return average
 
-    def average_scores(*args, **kwargs):
-        for key, value in kwargs.items():
-            print("%s == %s" % (key, value))
-        #return
+
+def average_scores(*avg, **kwargs):
+    result = 0
+    for x in avg:
+        result += x
+        average = sum(avg) / len(avg)
+        return average
+    for key, value in kwargs.items():
+        print("%s == %s" % (key, value))
 
 
-def place_holder(first_name, last_name, course_name):
-    print('first name: ' + first_name)
-    print('last_name: ' + last_name)
-    print('course name: ' + course_name)
-    print('test average: ' + average_scores_args_only())
-
-
-    # 'Result: name = M gpa = 3.2 course = Python with current average 30.0
+# 'Result: name = M gpa = 3.2 course = Python with current average 30.0
 
 
 if __name__ == '__main__':
-    average_scores_args_only(4, 2, 4, 5, 1, 3, 2, 5)
-    average_scores_args_only(5, 5, 5, 5, 5)
-    average_scores_args_only(1, 3, 1, 3, 1, 3)
-    average_scores_args_only(4, 4, 4, )
-    place_holder('george', 'ferguson', 'python')
-
+    kelly = {'Name': 'kelly', 'course': 'Python', 'gpa': average_scores_args_only(4, 3, 4, 4)}
+    leslie = {'Name': 'leslie', 'course': 'Database Design', 'gpa': average_scores_args_only(3, 2, 3, 4, 3.5, 2, 4, 3)}
+    shep = {'Name': 'shep', 'course': 'Java 2', 'gpa': average_scores_args_only(3, 4, 2.5, 4.2, 3.2, 1)}
+    average_scores(**kelly)
+    average_scores(**leslie)
+    average_scores(**shep)
+    #print(average_scores((4,3,3,4, name = )))
 
 #ky value pair
 #key is first name
